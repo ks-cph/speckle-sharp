8d4a1a497d148c6bed564a6a271fef4c332eabdd
Connor Ivy <connor@speckle.systems>
2023-04-03 15:16:19 -0500

    try starting program programmatically

--- ConnectorCSI/DriverCSharp/Program.cs
+++ ConnectorCSI/DriverCSharp/Program.cs
@@ -16,100 +16,21 @@ namespace DriverCSharp
 
     static int Main(string[] args)
     {
-#if DEBUG
-      Debugger.Launch();
-#endif
-      //MessageBox.Show("Starting DriverCSharp");
-
-      // dimension the SapObject as cOAPI type
-      cOAPI mySapObject = null;
+//#if DEBUG
+//      Debugger.Launch();
+//#endif
 
       // Use ret to check if functions return successfully (ret = 0) or fail (ret = nonzero)
       int ret = -1;
 
       // create API helper object
-      cHelper myHelper = null;
-
-      try
-      {
-        myHelper = new Helper();
-      }
-      catch (Exception ex)
-      {
-        MessageBox.Show("Cannot create an instance of the Helper object: " + ex.Message);
-        ret = -1;
-        return ret;
-      }
-
-      // attach to a running program instance 
-      try
-      {
-        // get the active SapObject
-        // determine program type
-        string progID = null;
-        string[] arguments = Environment.GetCommandLineArgs();
-
-        if (arguments.Count() > 1)
-        {
-          string arg = arguments[1];
-          if (string.Compare(arg, "SAP2000", true) == 0)
-            progID = ProgID_SAP2000;
-          else if (string.Compare(arg, "ETABS", true) == 0)
-            progID = ProgID_ETABS;
-          else if (string.Compare(arg, "SAFE", true) == 0)
-            progID = ProgID_SAFE;
-          else if (string.Compare(arg, "CSiBridge", true) == 0)
-            progID = ProgID_CSiBridge;
-        }
-
-        if (progID != null)
-          mySapObject = myHelper.GetObject(progID);
-        else
-        {
-          // missing/unknown program type, try one by one
-          try
-          {
-            progID = ProgID_SAP2000;
-            mySapObject = myHelper.GetObject(progID);
-          }
-          catch (Exception ex)
-          {
-          }
-
-          if (mySapObject == null)
-          {
-            try
-            { 
-              progID = ProgID_ETABS;
-              mySapObject = myHelper.GetObject(progID);
-            }
-            catch (Exception ex)
-            { 
-            }
-          }
-          if (mySapObject == null)
-          {
-            try
-            {
-              progID = ProgID_CSiBridge;
-              mySapObject = myHelper.GetObject(progID);
-            }
-            catch (Exception ex)
-            {
-            }
-          }
-        }
-      }
-      catch (Exception ex)
-      {
-        MessageBox.Show("No running instance of the program found or failed to attach: " + ex.Message);
-
-        ret = -2;
-        return ret;
-      }
+      cHelper myHelper = new Helper();
+      var sapObject = myHelper.CreateObjectProgID(ProgID_CSiBridge);
+      ret = sapObject.ApplicationStart();
+      ret = sapObject.SetAsActiveObject();
 
       // Get a reference to cSapModel to access all API classes and functions
-      cSapModel mySapModel = mySapObject.SapModel;
+      cSapModel mySapModel = sapObject.SapModel;
 
       // call Speckle plugin
       try
